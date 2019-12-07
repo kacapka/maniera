0727b970301c20a9f610b5d3cafde59e3c5ecd74
kacapka <wojciech.urbansk@gmail.com>
2019-12-07 10:37:20 +0100

    circle anim

--- components/Circle.js
+++ components/Circle.js
@@ -4,15 +4,15 @@ const Circle = ({ text }) => (
             <title>Maniera</title>
             <defs>
                 <path d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250" id="textcircle">
-                    {/* <animateTransform
-                    attributeName="transform"
-                    begin="0s"
-                    dur="30s"
-                    type="rotate"
-                    from="0 250 250"
-                    to="360 250 250"
-                    repeatCount="indefinite"
-                /> */}
+                    <animateTransform
+                        attributeName="transform"
+                        begin="0s"
+                        dur="30s"
+                        type="rotate"
+                        from="0 250 250"
+                        to="360 250 250"
+                        repeatCount="indefinite"
+                    />
                 </path>
             </defs>
             <text dy="-20">
